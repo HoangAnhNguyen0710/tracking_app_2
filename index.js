5a71a20e14549dadff6f042fdffcf10a4c92a1b1
HoangAnhNguyen0710 <HoangAnhNguyen07102001@gmail.com>
2024-12-21 23:26:58 +0700

    update host port

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.post("/tracking", async (req, res) => {
     }
   });
 
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
